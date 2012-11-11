46fd2ecdcb6dc1dd49b559acceb9e595c5fc56f7
oliversong <oliversong@tumblr.com>
2012-11-11 15:08:27 -0500

    Fixing app_ctx problems yay

--- badassery.py
+++ badassery.py
@@ -23,20 +23,29 @@ app.config.from_object(__name__)
 
 def get_db():
     # database connection
-    dbt = sqlite3.connect(app.config['DATABASE'])
-    dbt.row_factory = sqlite3.Row
-    return dbt
+    top = _app_ctx_stack.top
+    if not hasattr(top, 'sqlite_db'):
+        top.sqlite_db = sqlite3.connect(app.config['DATABASE'])
+        top.sqlite_db.row_factory = sqlite3.Row
+    return top.sqlite_db 
 
 @app.teardown_appcontext
 def close_database(exception):
     # Closes database at end of request
-    g.db.close()
+    top = _app_ctx_stack.top
+    if hasattr(top, 'sqlite_db'):
+        top.sqlite_db.close()
 
 def init_db():
-    with closing(get_db()) as db:
+    with app.app_context():
+        db = get_db()
         with app.open_resource('schema.sql') as f:
             db.cursor().executescript(f.read())
         db.commit()
+    # with closing(get_db()) as db:
+    #     with app.open_resource('schema.sql') as f:
+    #         db.cursor().executescript(f.read())
+    #     db.commit()
 
         db.execute('''insert into mod (
         pw_hash) values (?)''',
@@ -55,7 +64,6 @@ def before_request():
     g.mod = None
     if 'mod_id' in session:
         g.mod = query_db('select * from mod where mod_id = ?', [session['mod_id']], one=True)
-    g.db = get_db()
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
